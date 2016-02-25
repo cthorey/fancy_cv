450d141b82c296253e8280a5fa81494b07d10eee
cthorey <clement.thorey@gmail.com>
2016-02-25 08:51:45 +0100

    First commit

---
+++ auto/cv.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "cv"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "friggeri-cv"
+    "friggeri-cv10"
+    "afterpage"
+    "hyperref"
+    "color"
+    "xcolor")
+   (LaTeX-add-bibliographies
+    "bibliography")
+   (LaTeX-add-color-definecolors
+    "pblue"))
+ :latex)
+
